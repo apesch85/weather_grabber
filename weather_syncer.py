72fad60854307786e0694a14f61008a3eb0659fe
gorrilla86 <gorrilla86@gmail.com>
2020-10-14 16:21:31 -0400

    Fist working version

--- weather_syncer.py
+++ weather_syncer.py
@@ -22,20 +22,31 @@ def GetStats():
     temperature = get_body[get_body.index('Temperature:') + 1]
     wind_list = get_body[get_body.index('Wind:') + 1: get_body.index('MPH') + 1]
     wind = [detail for detail in wind_list if not detail.startswith('(') and not detail.endswith(')')]
+    wind_string = ' '.join(wind)
     pressure = get_body[get_body.index('Hg') + 1][1:]
 
-    stats_list = [temperature, wind, pressure]
+    stats_list = [temperature, wind_string, pressure]
 
     return stats_list
 
 
 def WriteSheet(stats):
+  count = 1
+  populated_cell = True
+  today_date = datetime.now().strftime("%m-%d-%Y")
+  today_time = datetime.now().strftime("%H:%M:%S")
   gc = gspread.service_account(filename=service_account)
   sheet = gc.open('Jackson Weather Tracker')
   worksheet = sheet.worksheet('Weather')
-  val = worksheet.acell('A1').value
-  print(val)
-
+  while populated_cell == True:
+    count += 1
+    populated_cell =  bool(worksheet.acell('A%s' % count).value)
+ 
+  worksheet.update('A%s' % count, today_date)
+  worksheet.update('B%s' % count, today_time)
+  worksheet.update('C%s' % count, stats[0])
+  worksheet.update('D%s' % count, stats[1])
+  worksheet.update('E%s' % count, stats[2])
 
 def main():
   weather_stats = GetStats()
